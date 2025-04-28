349163342baf1d30469a95a4dbf351faa78a13e7
Fr4nciscoD1az <fr4nciscollld1az@gmail.com>
2025-04-28 08:30:34 -0500

    Update add_event_controller.dart

--- lib/presentation/screens/add_event/add_event_controller.dart
+++ lib/presentation/screens/add_event/add_event_controller.dart
@@ -2,18 +2,15 @@ import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 
 class AddEventController extends GetxController {
-  // Campos
   final nombreEvento = ''.obs;
-  final mascotaSeleccionada = 'Firulais'.obs;
-  final tipoLugar = 'manual'.obs; // 'manual' o 'veterinaria'
+  final mascotaSeleccionada = ''.obs;
+  final tipoLugar = 'manual'.obs;
   final lugarEvento = ''.obs;
   final veterinariaSeleccionada = ''.obs;
+  final fechaEvento = Rxn<DateTime>(); // Puede ser nulo
+  final horaEvento = Rxn<TimeOfDay>(); // Puede ser nulo
+  final categoriaEvento = ''.obs;
 
-  final fechaEvento = DateTime.now().obs;
-  final horaEvento = TimeOfDay.now().obs;
-  final categoriaEvento = 'Baño'.obs;
-
-  // Opciones
   final categorias = ['Baño', 'Veterinario', 'Medicina', 'Vacuna'].obs;
   final veterinarias = ['Veterinaria Paws', 'Centro AnimalCare', 'PetClinic Plus'].obs;
   final mascotas = ['Firulais', 'Mishi', 'Kira', 'Luna'].obs;
@@ -23,8 +20,9 @@ class AddEventController extends GetxController {
     tipoLugar.value = 'manual';
     lugarEvento.value = '';
     veterinariaSeleccionada.value = '';
-    fechaEvento.value = DateTime.now();
-    horaEvento.value = TimeOfDay.now();
-    categoriaEvento.value = 'Baño';
+    fechaEvento.value = null;
+    horaEvento.value = null;
+    categoriaEvento.value = '';
+    mascotaSeleccionada.value = '';
   }
 }
