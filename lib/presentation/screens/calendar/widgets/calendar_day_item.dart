9f90194b186328141f265300d179bbfd1d53f471
Fr4nciscoD1az <fr4nciscollld1az@gmail.com>
2025-04-28 08:27:02 -0500

    Add files via upload

---
+++ lib/presentation/screens/calendar/widgets/calendar_day_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:animacare_front/presentation/theme/colors.dart'; // << Importación corregida
+
+class CalendarDayItem extends StatelessWidget {
+  final DateTime day;
+  final bool isSelected;
+  final bool isOverloaded;
+
+  const CalendarDayItem({
+    Key? key,
+    required this.day,
+    required this.isSelected,
+    required this.isOverloaded,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Color backgroundColor;
+    Color textColor;
+
+    if (isSelected) {
+      backgroundColor = isOverloaded ? AppColors.markerBorder : AppColors.selectedDay;
+      textColor = AppColors.primaryWhite;
+    } else if (isOverloaded) {
+      backgroundColor = AppColors.transparent;
+      textColor = Colors.black;
+    } else {
+      backgroundColor = AppColors.transparent;
+      textColor = Colors.black;
+    }
+
+    return Container(
+      margin: const EdgeInsets.all(6.0),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        shape: BoxShape.circle,
+        border: isOverloaded && !isSelected
+            ? Border.all(color: AppColors.markerBorder, width: 2)
+            : null,
+      ),
+      child: Text(
+        '${day.day}',
+        style: TextStyle(
+          color: textColor,
+          fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/screens/calendar/widgets/month_year_picker.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../calendar_controller.dart';
+import 'package:animacare_front/presentation/theme/colors.dart'; // << Importación correcta
+
+class MonthPickerSheet extends StatelessWidget {
+  final CalendarController controller;
+
+  const MonthPickerSheet({Key? key, required this.controller}) : super(key: key);
+
+  String _monthName(int month) {
+    const months = [
+      '', 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+      'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+    ];
+    return months[month];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: const EdgeInsets.all(20),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        childAspectRatio: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemCount: 12,
+      itemBuilder: (context, index) {
+        return ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: AppColors.formatButtonBackground,
+            foregroundColor: AppColors.formatButtonForeground,
+            textStyle: const TextStyle(fontWeight: FontWeight.bold),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          ),
+          onPressed: () {
+            controller.cambiarMes(index + 1);
+            Navigator.pop(context);
+          },
+          child: Text(_monthName(index + 1)),
+        );
+      },
+    );
+  }
+}
+
+class YearPickerSheet extends StatelessWidget {
+  final CalendarController controller;
+
+  const YearPickerSheet({Key? key, required this.controller}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(20),
+      itemCount: 11,
+      itemBuilder: (context, index) {
+        final year = 2020 + index;
+        return ListTile(
+          title: Text(
+            '$year',
+            style: const TextStyle(
+              color: AppColors.formatButtonForeground, // Coloreamos también los años
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          onTap: () {
+            controller.cambiarAnio(year);
+            Navigator.pop(context);
+          },
+        );
+      },
+    );
+  }
+}
