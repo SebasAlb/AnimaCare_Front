2abc64904b20123946ea42c55b5517a05c6f01c6
Fr4nciscoD1az <fr4nciscollld1az@gmail.com>
2025-05-18 12:08:32 -0500

    Update home_controller.dart

--- lib/presentation/screens/home/Mascota_Principal/home_controller.dart
+++ lib/presentation/screens/home/Mascota_Principal/home_controller.dart
@@ -1,18 +1,56 @@
-import 'package:animacare_front/presentation/screens/home/Agregar_Mascota/agregar_mascota_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animacare_front/models/mascota.dart';
+import 'package:animacare_front/presentation/screens/home/Agregar_Mascota/agregar_mascota_screen.dart';
 
-class HomeController {
-  final List<String> mascotas = <String>[
-    'Gato 1',
-    'Gato 2',
-    'Gato 3',
-    'Perro 1',
-  ];
+class HomeController extends ChangeNotifier {
+  final List<Mascota> _mascotas = [];
 
-  void onAgregarMascota(BuildContext context) {
-    Navigator.push(
+  List<Mascota> get mascotas => _mascotas;
+
+  void cargarMascotasIniciales() {
+    _mascotas.clear();
+    _mascotas.addAll([
+      Mascota(
+        id: '1',
+        nombre: 'Michi',
+        especie: 'Gato',
+        raza: 'Siames',
+        fechaNacimiento: DateTime(2021, 5, 10),
+        sexo: 'Macho',
+        peso: 4.2,
+        altura: 30.0,
+        fotoUrl: '', // puedes usar un asset de momento
+      ),
+      Mascota(
+        id: '2',
+        nombre: 'Firulais',
+        especie: 'Perro',
+        raza: 'Labrador',
+        fechaNacimiento: DateTime(2020, 2, 3),
+        sexo: 'Hembra',
+        peso: 20.5,
+        altura: 60.0,
+        fotoUrl: '',
+      ),
+    ]);
+    notifyListeners();
+  }
+
+  void agregarMascota(Mascota nueva) {
+    _mascotas.add(nueva);
+    notifyListeners();
+  }
+
+  void onAgregarMascota(BuildContext context) async {
+    final Mascota? nueva = await Navigator.push(
       context,
-      MaterialPageRoute(builder: (_) => const AgregarMascotaScreen()),
+      MaterialPageRoute(
+        builder: (_) => const AgregarMascotaScreen(),
+      ),
     );
+
+    if (nueva != null) {
+      agregarMascota(nueva);
+    }
   }
 }
