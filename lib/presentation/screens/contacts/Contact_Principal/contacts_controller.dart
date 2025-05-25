e289436b5422371ec74b4db03ca03833e68513e1
Fr4nciscoD1az <fr4nciscollld1az@gmail.com>
2025-05-24 19:02:42 -0500

    Update contacts_controller.dart

--- lib/presentation/screens/contacts/Contact_Principal/contacts_controller.dart
+++ lib/presentation/screens/contacts/Contact_Principal/contacts_controller.dart
@@ -5,11 +5,14 @@ import 'package:animacare_front/models/veterinario_excepcion.dart';
 import 'package:animacare_front/presentation/screens/contacts/Agendar_Cita/agendar_cita_screen.dart';
 import 'package:animacare_front/presentation/screens/contacts/Contacto_Detalle/contact_info_screen.dart';
 
+import 'package:animacare_front/presentation/components/list_extensions.dart';
+
 class ContactsController {
+  
   final List<Veterinario> contactos = [
     Veterinario(
       id: '1',
-      nombre: 'Lucía',
+      nombre: 'Ana',
       apellido: 'Andrade',
       cedulaProfesional: 'VET123456',
       telefono: '+593 987 654 321',
@@ -24,7 +27,7 @@ class ContactsController {
         'Miércoles': '10:00 - 20:00',
         'Jueves': '08:00 - 16:00',
         'Viernes': '08:00 - 16:00',
-        'Sábado': '09:00 - 13:00',
+        'Sábado': '09:00 - 14:00',
         'Domingo': 'Cerrado',
       },
       horariosDetalle: [
@@ -33,6 +36,7 @@ class ContactsController {
         VeterinarioHorario(veterinarioId: '1', diaSemana: 'Miércoles', horaInicio: '10:00', horaFin: '20:00'),
         VeterinarioHorario(veterinarioId: '1', diaSemana: 'Jueves', horaInicio: '08:00', horaFin: '16:00'),
         VeterinarioHorario(veterinarioId: '1', diaSemana: 'Viernes', horaInicio: '08:00', horaFin: '16:00'),
+        VeterinarioHorario(veterinarioId: '1', diaSemana: 'Sábado', horaInicio: '09:00', horaFin: '14:00'),
       ],
     ),
     Veterinario(
@@ -103,10 +107,10 @@ class ContactsController {
       disponible: false,
     ),
     VeterinarioExcepcion(
-      veterinarioId: '3',
-      fechaInicio: DateTime.now().subtract(const Duration(hours: 2)),
-      fechaFin: DateTime.now().add(const Duration(hours: 5)),
-      motivo: 'Congreso médico',
+      veterinarioId: '3', // o el ID del veterinario que quieras
+      fechaInicio: DateTime(2025, 5, 26, 8), // 26 de mayo a las 08:00
+      fechaFin: DateTime(2025, 5, 26, 12),   // 26 de mayo a las 12:00
+      motivo: 'Capacitación interna',
       disponible: false,
     ),
   ];
@@ -130,4 +134,59 @@ class ContactsController {
       MaterialPageRoute(builder: (_) => const AgendarCitaScreen()),
     );
   }
+
+  Veterinario? obtenerVeterinarioPorNombre(String nombreCompleto) {
+    return contactos.firstWhereOrNull((v) => v.nombreCompleto == nombreCompleto);
+  }
+  
+  List<String> obtenerHorasDisponiblesParaFecha({
+    required String nombreVeterinario,
+    required DateTime fechaSeleccionada,
+    required List<String> horasOcupadasSimuladas,
+  }) {
+    final vet = obtenerVeterinarioPorNombre(nombreVeterinario);
+    if (vet == null) return [];
+
+    final diaNombre = _nombreDia(fechaSeleccionada.weekday);
+
+    final horario = vet.horariosDetalle?.firstWhereOrNull(
+      (h) => h.diaSemana == diaNombre,
+    );
+    if (horario == null) return [];
+
+    final bloques = _generarBloquesHorario(horario.horaInicio, horario.horaFin);
+
+    // Devuelve todos los bloques excepto los ocupados simulados
+    return bloques.where((bloque) => !horasOcupadasSimuladas.contains(bloque)).toList();
+  }
+
+
+  String _nombreDia(int weekday) {
+    const dias = [
+      'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'
+    ];
+    return dias[weekday - 1];
+  }
+
+  
+  List<String> _generarBloquesHorario(String horaInicio, String horaFin) {
+    final int start = int.parse(horaInicio.split(':')[0]);
+    final int end = int.parse(horaFin.split(':')[0]);
+
+    final List<String> bloques = [];
+
+    for (int i = start; i < end; i++) {
+      final String bloqueInicio = '${i.toString().padLeft(2, '0')}:00';
+      final String bloqueFin = '${(i + 1).toString().padLeft(2, '0')}:00';
+
+      // Excluir el bloque que incluye 12:00 (almuerzo)
+      if (bloqueInicio != '12:00') {
+        bloques.add('$bloqueInicio - $bloqueFin');
+      }
+    }
+
+    return bloques;
+  }
+
 }
+
