b4550e64e4317bc9c9ab246d6d3726d8c760d25e
Fr4nciscoD1az <fr4nciscollld1az@gmail.com>
2025-05-27 00:26:01 -0500

    Update contact_info_screen.dart

--- lib/presentation/screens/contacts/Contacto_Detalle/contact_info_screen.dart
+++ lib/presentation/screens/contacts/Contacto_Detalle/contact_info_screen.dart
@@ -37,127 +37,118 @@ class ContactInfoScreen extends StatelessWidget {
               nameScreen: 'Contactos',
               isSecondaryScreen: true,
             ),
+
             Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: theme.colorScheme.onPrimary.withOpacity(0.2),
-                    width: 8,
-                  ),
-                ),
-                child: SingleChildScrollView(
-                  padding: const EdgeInsets.all(20),
-                  child: Column(
-                    children: <Widget>[
-                      Text(
-                        'Veterinario',
-                        style: theme.textTheme.titleLarge?.copyWith(
-                          color: theme.colorScheme.primary,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 1.2,
-                        ),
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      'Veterinario',
+                      style: theme.textTheme.titleLarge?.copyWith(
+                        color: theme.colorScheme.primary,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.2,
                       ),
-                      const SizedBox(height: 6),
-                      Text(
-                        veterinario.nombreCompleto,
-                        style: theme.textTheme.titleMedium?.copyWith(
-                          color: theme.colorScheme.primary, // original: theme.colorScheme.onPrimary,
-                        ),
+                    ),
+                    const SizedBox(height: 6),
+                    Text(
+                      veterinario.nombreCompleto,
+                      style: theme.textTheme.titleMedium?.copyWith(
+                        color: theme.colorScheme.primary,
                       ),
-                      const SizedBox(height: 25),
-                      CircleAvatar(
-                        radius: 65,
-                        backgroundColor: theme.colorScheme.primary, //original: theme.cardColor,
-                        backgroundImage: veterinario.fotoUrl.isNotEmpty
-                            ? NetworkImage(veterinario.fotoUrl)
-                            : null,
-                        child: veterinario.fotoUrl.isEmpty
-                            ? Icon(Icons.person, size: 60, color: theme.cardColor /*color: theme.iconTheme.color*/)
-                            : null,
-                      ),
-                      const SizedBox(height: 25),
-                      Column(
-                        children: <Widget>[
-                          _infoLinea(theme, Icons.phone, veterinario.telefono),
-                          const SizedBox(height: 8),
-                          _infoLinea(theme, Icons.email, veterinario.correo),
-                        ],
-                      ),
-                      const SizedBox(height: 30),
-
-                      // ✅ Mostrar disponibilidad primero
-                      if (excepcionesRelevantes.isNotEmpty) ...[
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            'Próximas ausencias',
-                            style: theme.textTheme.bodyLarge?.copyWith(
-                              color: theme.colorScheme.primary,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
+                    ),
+                    const SizedBox(height: 25),
+                    CircleAvatar(
+                      radius: 65,
+                      backgroundColor: theme.colorScheme.primary,
+                      backgroundImage: veterinario.fotoUrl.isNotEmpty
+                          ? NetworkImage(veterinario.fotoUrl)
+                          : null,
+                      child: veterinario.fotoUrl.isEmpty
+                          ? Icon(Icons.person, size: 60, color: theme.cardColor)
+                          : null,
+                    ),
+                    const SizedBox(height: 25),
+                    Column(
+                      children: <Widget>[
+                        _infoLinea(theme, Icons.phone, veterinario.telefono),
                         const SizedBox(height: 8),
-                        ...excepcionesRelevantes.map((e) {
-                          final bool esHoy = DateTime.now().isAfter(e.fechaInicio) && DateTime.now().isBefore(e.fechaFin);
-                          final Color fondo = esHoy ? Colors.redAccent.withOpacity(0.1) : Colors.amber.withOpacity(0.15);
-                          final Color icono = esHoy ? Colors.redAccent : Colors.amber[700]!;
-                          final String textoEstado = esHoy ? 'No disponible por' : 'No estará disponible por';
-
-                          return Container(
-                            margin: const EdgeInsets.only(bottom: 12),
-                            padding: const EdgeInsets.all(12),
-                            decoration: BoxDecoration(
-                              color: fondo,
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            child: Row(
-                              children: [
-                                Icon(Icons.warning_amber_rounded, color: icono),
-                                const SizedBox(width: 10),
-                                Expanded(
-                                  child: Text(
-                                    '$textoEstado: ${e.motivo}\n'
-                                    'Desde: ${DateFormat('dd/MM/yyyy HH:mm').format(e.fechaInicio)}\n'
-                                    'Hasta: ${DateFormat('dd/MM/yyyy HH:mm').format(e.fechaFin)}',
-                                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                                      color: icono,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          );
-                        }),
-                        const SizedBox(height: 25),
+                        _infoLinea(theme, Icons.email, veterinario.correo),
                       ],
-
-
+                    ),
+                    const SizedBox(height: 30),
+                    if (excepcionesRelevantes.isNotEmpty) ...[
                       Align(
                         alignment: Alignment.centerLeft,
                         child: Text(
-                          'Horario de atención',
+                          'Próximas ausencias',
                           style: theme.textTheme.bodyLarge?.copyWith(
                             color: theme.colorScheme.primary,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
                       ),
-                      const SizedBox(height: 12),
-                      Container(
-                        padding: const EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          color: theme.colorScheme.primary.withOpacity(0.1),/*original: theme.colorScheme.onPrimary*/
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: HorarioTable(horarios: veterinario.horario),
-                      ),
-                      const SizedBox(height: 90),
+                      const SizedBox(height: 8),
+                      ...excepcionesRelevantes.map((e) {
+                        final bool esHoy = DateTime.now().isAfter(e.fechaInicio) && DateTime.now().isBefore(e.fechaFin);
+                        final Color fondo = esHoy ? Colors.redAccent.withOpacity(0.1) : Colors.amber.withOpacity(0.15);
+                        final Color icono = esHoy ? Colors.redAccent : Colors.amber[700]!;
+                        final String textoEstado = esHoy ? 'No disponible por' : 'No estará disponible por';
+
+                        return Container(
+                          margin: const EdgeInsets.only(bottom: 12),
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: fondo,
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Row(
+                            children: [
+                              Icon(Icons.warning_amber_rounded, color: icono),
+                              const SizedBox(width: 10),
+                              Expanded(
+                                child: Text(
+                                  '$textoEstado: ${e.motivo}\n'
+                                  'Desde: ${DateFormat('dd/MM/yyyy HH:mm').format(e.fechaInicio)}\n'
+                                  'Hasta: ${DateFormat('dd/MM/yyyy HH:mm').format(e.fechaFin)}',
+                                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                                    color: icono,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }),
+                      const SizedBox(height: 25),
                     ],
-                  ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Horario de atención',
+                        style: theme.textTheme.bodyLarge?.copyWith(
+                          color: theme.colorScheme.primary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    Container(
+                      padding: const EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: theme.colorScheme.primary.withOpacity(0.1),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: HorarioTable(horarios: veterinario.horario),
+                    ),
+                    const SizedBox(height: 90),
+                  ],
                 ),
               ),
             ),
+
+
           ],
         ),
       ),
@@ -219,6 +210,11 @@ class HorarioTable extends StatelessWidget {
     );
   }
 }
+
+
+
+
+
 
 
 
