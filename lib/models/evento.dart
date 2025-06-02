213801f2a82eee533688b32562fa1fdeb4c848ee
Fr4nciscoD1az <fr4nciscollld1az@gmail.com>
2025-06-02 01:44:37 -0500

    Update evento.dart

--- lib/models/evento.dart
+++ lib/models/evento.dart
@@ -1,3 +1,6 @@
+import 'veterinario.dart'; // asegúrate de importar este modelo
+import 'categoria_atencion.dart';
+
 class Evento {
   int id;
   String titulo;
@@ -7,6 +10,8 @@ class Evento {
   int? categoriaId;
   int? mascotaId;
   int? veterinarioId;
+  Veterinario? veterinario; // ✅ NUEVO
+  CategoriaAtencion? categoria; // ✅ NUEVO
 
   Evento({
     required this.id,
@@ -17,28 +22,41 @@ class Evento {
     this.categoriaId,
     this.mascotaId,
     this.veterinarioId,
+    this.veterinario, // ✅
+    this.categoria,
   });
 
   factory Evento.fromJson(Map<String, dynamic> json) => Evento(
-    id: json['id'],
-    titulo: json['titulo'] ?? '',
-    fecha: DateTime.parse(json['fecha']),
-    hora: DateTime.parse(json['hora']),
-    descripcion: json['descripcion'],
-    categoriaId: json['categoria_id'],
-    mascotaId: json['mascota_id'],
-    veterinarioId: json['veterinario_id'],
-  );
+        id: json['id'],
+        titulo: json['titulo'] ?? '',
+        fecha: DateTime.parse(json['fecha']),
+        hora: DateTime.parse(json['hora']),
+        descripcion: json['descripcion'],
+        categoriaId: json['categoria']?['id'] ?? json['categoria_id'],
+        mascotaId: json['mascota_id'],
+        veterinarioId: json['veterinario']?['id'] ?? json['veterinario_id'],
+        veterinario: json['veterinario'] != null
+            ? Veterinario.fromJson({
+                ...json['veterinario'],
+                'horarios': [], // para evitar error si backend no los manda
+                'excepciones': [],
+              })
+            : null,
+        categoria: json['categoria'] != null
+          ? CategoriaAtencion.fromJson(json['categoria'])
+          : null,
+      );
 
   Map<String, dynamic> toJson() => {
-    'id': id,
-    'titulo': titulo,
-    'fecha': fecha.toIso8601String(),
-    'hora': hora.toIso8601String(),
-    'descripcion': descripcion,
-    'categoria_id': categoriaId,
-    'mascota_id': mascotaId,
-    'veterinario_id': veterinarioId,
-  };
-  
+        'id': id,
+        'titulo': titulo,
+        'fecha': fecha.toIso8601String(),
+        'hora': hora.toIso8601String(),
+        'descripcion': descripcion,
+        'categoria_id': categoriaId,
+        'mascota_id': mascotaId,
+        'veterinario_id': veterinarioId,
+        // ❌ NO incluir `veterinario` al enviar, solo el ID
+      };
 }
+
